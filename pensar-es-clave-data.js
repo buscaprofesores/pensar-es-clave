6b0e8ab8de625db6adf7506f819607d8490aca90
BuscaProfesores Chile <buscaprofesores@users.noreply.github.com>
2018-01-03 18:55:26 -0300

    Update pensar-es-clave-data.js

--- pensar-es-clave-data.js
+++ pensar-es-clave-data.js
@@ -3,57 +3,25 @@ var treeData = [
     "name": "Pensar es Clave, Federico Sánchez",
     "parent": "null",
     "children": [
-      {
-            "name": "Septiembre"
-            ,"_children": [
-               {  
-                "name":   "viernes 1 - Fabrizio Copano",
-                 "url": "https://www.youtube.com/v/kDlTMDMYo-U"
-              }
-             ,{  
-                "name":   "Mentes Brillantes 1 - Ricardo Baeza",
-                 "url": "https://www.youtube.com/v/36BXjU7qvPQ"
-              },
-            
-             { "name":    "Mentes Brillantes 1 - Fernando Mönckeberg"
-             ,"url":   "https://www.youtube.com/v/BNas0FvnIyk"
-             },
-          
-              {
-              "name":    "Mentes Brillantes 1 - Ramón Latorre"
-              ,"url":   "https://www.youtube.com/v/voogNkB14gM"
-              }
- 
-]
- 
-},
-    
-{
-            "name": "Temporada 2"
-            ,"_children": [
-               {  
-                "name":   "Mentes Brillantes 2 - Jorge Allende",
-                 "url": "https://www.youtube.com/v/joXU0YgKu4Q"
-              }
-              ,{  
-                "name":   "Mentes Brillantes 2 - Cecilia Hidalgo",
-                 "url": "https://www.youtube.com/v/BENmwqYWRVM"
-              },
-              
-              {  
-                "name":   "Mentes Brillantes 2 - Andrés Weintraub",
-                 "url": "https://www.youtube.com/v/UoPAdtI8HRU"
-              },
-              {"name":    "Mentes Brillantes 2 - Ricardo Uauy"
-              ,"url":   "https://www.youtube.com/v/oNgIp0ifo0s"
-              }
-
- 
-]
- 
-}
-       
-
+      {     "name": "Julio",
+            "_children": [
+             { "name":   "jueves 20 - Joe Vascncellos",   "url": "https://www.youtube.com/v/jKKM-KMyQvo"      },      
+             { "name":    "Mentes Brillantes 1 - Ramón Latorre"  ,"url":   "https://www.youtube.com/v/voogNkB14gM"    }
+           ]
+      },
+      {     "name": "Septiembre",
+            "_children": [
+             { "name":   "viernes 1 - Fabrizio Copano",   "url": "https://www.youtube.com/v/kDlTMDMYo-U"      },      
+             { "name":    "Mentes Brillantes 1 - Ramón Latorre"  ,"url":   "https://www.youtube.com/v/voogNkB14gM"    }
+           ]
+      },
+    {       "name": "Octubre",
+            "_children": [
+             { "name":   "Mentes Brillantes 2 - Jorge Allende",        "url": "https://www.youtube.com/v/joXU0YgKu4Q"  },
+             {"name":    "Mentes Brillantes 2 - Ricardo Uauy"     ,"url":   "https://www.youtube.com/v/oNgIp0ifo0s"     }
+            ]
+     }
+  
     ]
   }
 ];
