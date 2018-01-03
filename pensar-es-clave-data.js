80ff5b4de0da794dc53e7ffc7fd3ebb87a676360
BuscaProfesores Chile <buscaprofesores@users.noreply.github.com>
2018-01-03 18:34:23 -0300

    Create pensar-es-clave-data.js

---
+++ pensar-es-clave-data.js
@@ -0,0 +1,110 @@
+var treeData = [
+  {
+    "name": "MentesBrillantes",
+    "parent": "null",
+    "children": [
+      {
+            "name": "Temporada 1"
+            ,"_children": [
+               {  
+                "name":   "Mentes Brillantes 1 - Pablo Valenzuela",
+                 "url": "https://www.youtube.com/v/ZmzUksziaCg"
+              }
+             ,{  
+                "name":   "Mentes Brillantes 1 - Ricardo Baeza",
+                 "url": "https://www.youtube.com/v/36BXjU7qvPQ"
+              },
+              {  
+                "name":   "Mentes Brillantes 1 - Carlos Conca",
+                 "url": "https://www.youtube.com/v/nRPOUSD1GU8"
+              }
+              ,
+              {  
+                "name":   "Mentes Brillantes 1 - Eric Goles",
+                 "url": "https://www.youtube.com/v/Nn0lXUqjhek"
+              }
+               ,
+              {  
+                "name":   "Mentes Brillantes 1 - Nibaldo Inestroza",
+                 "url": "https://www.youtube.com/v/lqfKONeRNg4"
+              }
+               ,
+              {  
+                "name":   "Mentes Brillantes 1 - Juan Asenjo",
+                 "url": "https://www.youtube.com/v/bWYJC4BzU6Y"
+              },
+              
+             {
+                "name":    "Mentes Brillantes 1 - María Teresa Ruiz"
+                ,"url":   "https://www.youtube.com/v/LYzGmPmu0vg"
+             },
+
+             { "name":    "Mentes Brillantes 1 - Mario Luxoro"
+             ,"url":   "https://www.youtube.com/v/YEUwSUePz5k"
+             } , 
+
+             { "name":    "Mentes Brillantes 1 - Fernando Mönckeberg"
+             ,"url":   "https://www.youtube.com/v/BNas0FvnIyk"
+             },
+          
+              {
+              "name":    "Mentes Brillantes 1 - Ramón Latorre"
+              ,"url":   "https://www.youtube.com/v/voogNkB14gM"
+              }
+ 
+]
+ 
+},
+    
+{
+            "name": "Temporada 2"
+            ,"_children": [
+               {  
+                "name":   "Mentes Brillantes 2 - Jorge Allende",
+                 "url": "https://www.youtube.com/v/joXU0YgKu4Q"
+              }
+              ,{  
+                "name":   "Mentes Brillantes 2 - Cecilia Hidalgo",
+                 "url": "https://www.youtube.com/v/BENmwqYWRVM"
+              },
+              {  
+                "name":   "Mentes Brillantes 2 - Juan Carlos Castilla",
+                 "url": "https://www.youtube.com/v/gKeUZgfPqC4"
+              },
+              {  
+                "name":   "Mentes Brillantes 2 - Fernando Lund",
+                 "url": "https://www.youtube.com/v/5LjWiK58Qus"
+              },
+             {  
+                "name":   "Mentes Brillantes 2 - Mary Kalin",
+                 "url": "https://www.youtube.com/v/d8Rzc3g2_2I"
+              },
+              {  
+                "name":   "Mentes Brillantes 2 - Patricio Felmer",
+                 "url": "https://www.youtube.com/v/NbxWbgtr0Ok"
+              },
+              {  
+                "name":   "Mentes Brillantes 2 - Enrique Tirapegui",
+                 "url": "https://www.youtube.com/v/nKxFf4MMWbQ"
+              },
+              {  
+                "name":   "Mentes Brillantes 2 - Bernabé Santelices",
+                 "url": "https://www.youtube.com/v/1BqLqGkOkZ8"
+              },
+              {  
+                "name":   "Mentes Brillantes 2 - Andrés Weintraub",
+                 "url": "https://www.youtube.com/v/UoPAdtI8HRU"
+              },
+              {"name":    "Mentes Brillantes 2 - Ricardo Uauy"
+              ,"url":   "https://www.youtube.com/v/oNgIp0ifo0s"
+              }
+
+ 
+]
+ 
+}
+       
+
+    ]
+  }
+];
